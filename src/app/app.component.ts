e6cf31ccbda76714e0f22055cbac800035e8fdd2
krzysztof.klich <kklich97@gmail.com>
2025-06-16 18:09:54 +0200

    add google tag

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CurriculumVitaeComponent } from "./components/curriculum-vitae/curriculum-vitae.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  imports: [CurriculumVitaeComponent]
 })
 export class AppComponent {
   title = 'Krzysztof Klich';
---
+++ src/app/components/curriculum-vitae/curriculum-vitae.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-curriculum-vitae',
+  imports: [],
+  templateUrl: './curriculum-vitae.component.html',
+  styleUrl: './curriculum-vitae.component.scss'
+})
+export class CurriculumVitaeComponent {
+
+}
