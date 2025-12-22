863359eb1a4f1559a47011f5067b9aca355e5e49
krzysztof <krzysztof.klich@idealogic.pl>
2025-12-22 09:29:42 +0100

    fix routing

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,11 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-import { serverRoutes } from './app.routes.server';
 import { routes } from './app.routes';
-import { provideServerRouting } from '@angular/ssr';
 import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
     providers: [provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideServerRouting(serverRoutes),
-    provideClientHydration(withEventReplay()),
-    provideHttpClient()]
+    provideHttpClient()
+    ]
 };
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -3,8 +3,6 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { DataPoint } from '../models/aggregated_data.model';
 
-
-
 @Injectable({
     providedIn: 'root'
 })
